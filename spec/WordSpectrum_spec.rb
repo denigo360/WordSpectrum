0c8a1d6e31fd157e634f3cbaab6e2827c42288ef
denigo360 <115095299+denigo360@users.noreply.github.com>
2024-06-03 13:06:33 +0300

    Fix some problems

---
+++ spec/WordSpectrum_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe WordSpectrum do
+  it "has a version number" do
+    expect(WordSpectrum::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
